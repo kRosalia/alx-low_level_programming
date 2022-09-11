8f21831152f66e8cf30dca8c04b833dd063fd959
kRosalia <rosaliahleezr@gmail.com>
2022-09-11 11:49:28 -0700

    printing various types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
 
-/*Prints size of each
-*wached from video
+/*
+* Prints size of each
+* answer watched from video
+*
+* main - Entry point
+*
+* Return: always 0 (Success)
 */
-
-
-
-int main(void)
+int main(void)/*function main*/
 {
 	int i;
 	long int lt;
 	char c;
 	long long int llt;
 	float f;
-	
+
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
-	printf("Size of a long int: %lu byte(s)\n",(unsigned long)sizeof(lt));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(lt));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(llt));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 
-	return(0);
-
+	return (0);
 }
