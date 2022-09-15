d810c439a840506a56bf4f329935b2b462ce6dfd
kRosalia <rosaliahleezr@gmail.com>
2022-09-15 02:43:14 -0700

    less print arguments

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 
-	printf("last digit of %d is %f and is ", n, lastDigit);
+	printf("last digit of %d is %d and is ", n, lastDigit);
 
 	if (lastDigit > 5)
 	{
