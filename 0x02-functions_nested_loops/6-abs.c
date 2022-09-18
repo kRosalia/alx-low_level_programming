8b3d3791cfef53ac6e30c65f11756eeab26bf0ba
kRosalia <rosaliahleezr@gmail.com>
2022-09-18 14:06:36 -0700

    computes absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - computes absolute figure man abs
+ * @n: checks integer
+ * Return: 0
+ */
+
+int _abs(int n)
+{
+
+	printf("%d", n);
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -35,5 +35,5 @@ int _isalpha(int c);
 
 int print_sign(int n);
 
-
+int _abs(int);
 #endif
